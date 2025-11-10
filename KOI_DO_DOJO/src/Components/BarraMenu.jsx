b49067f01ea011319efa0a537c2cc96c62b95a2e
amachado71421 <alemachadofwd@gmail.com>
2025-11-10 10:06:27 -0600

    Termino barra de navegacion

--- KOI_DO_DOJO/src/Components/BarraMenu.jsx
+++ KOI_DO_DOJO/src/Components/BarraMenu.jsx
@@ -1,55 +1,60 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import logo from "../Images/LOGO.png"
-import Barra from "../Styles/Barra.css"
+import "../styles/Barra.css";
 
 function BarraMenu() {
   const navigate = useNavigate()
   return (
-    <div className='Menu' >
-      <main>
-        <img className="Logo" src={logo} alt="Logo" />
-        <ul className="menu">
+    <div>
+      <main className='Menu'>
+        <img className="Logo" src="../src/Images/LOGO.png" alt="Logo" />
+        <ul className="Inicio Elementos">
           <li className="menu-item">
-            <img src="..\src\Images\INICIO.png" alt="IconoInicio" />
-            <span onClick={() => navigate("/")}>Inicio</span>
+            <img className="Icono" src="../src/Images/INICIO.png" alt="IconoInicio" />
+            <span className='Titulo' onClick={() => navigate("/")}>Inicio</span>
           </li>
+          <div class="linea-vertical"></div>
           <li className="menu-item">
-            <img src="..\src\Images\PERFIL2.png" alt="IconoPerfil" />
-            <span onClick={() => navigate("/Perfil")}>Perfil</span>
+            <img className="Icono" src="../src/Images/PERFIL2.png" alt="IconoPerfil" />
+            <span className='Titulo' onClick={() => navigate("/Perfil")}>Perfil</span>
           </li>
+          <div class="linea-vertical"></div>
           <li className="menu-item">
-            <img src="..\src\Images\NOSOTROS.png" alt="IconoExplorar" />
-            <span onClick={() => navigate("/QuienesSomos")}>Quienes Somos</span>
+            <img className="Icono" src="../src/Images/NOSOTROS.png" alt="IconoNosotros" />
+            <span className='Titulo' onClick={() => navigate("/QuienesSomos")}>¿Quienes Somos?</span>
           </li>
+          <div class="linea-vertical"></div>
           <li className="menu-item">
-            <img src="..\src\Images\DOJO.png" alt="IconoMatch" />
-            <span onClick={() => navigate("/Dojo")}>Dojo</span>
+            <img className="Icono" src="../src/Images/DOJO.png" alt="IconoDojo" />
+            <span className='Titulo' onClick={() => navigate("/Dojo")}>Dojo</span>
           </li>
+          <div class="linea-vertical"></div>
           <li className="menu-item">
-            <img src="..\src\Images\CALENDARIO.png" alt="IconoAjustes" />
-            <span onClick={() => navigate("/Calendario")}>Calendario</span>
+            <img className="Icono" src="../src/Images/CALENDARIO.png" alt="IconoCalendario" />
+            <span className='Titulo' onClick={() => navigate("/Calendario")}>Calendario</span>
           </li>
+          <div class="linea-vertical"></div>
           <li className="menu-item">
-            <img src="..\src\Images\EVENTOS.png" alt="IconoAjustes" />
-            <span onClick={() => navigate("/Eventos")}>Eventos</span>
+            <img className="Icono" src="../src/Images/EVENTOS.png" alt="IconoEventos" />
+            <span className='Titulo' onClick={() => navigate("/Eventos")}>Eventos</span>
           </li>
+          <div class="linea-vertical"></div>
           <li className="menu-item">
-            <img src="..\src\Images\RANKING.png" alt="IconoAjustes" />
-            <span onClick={() => navigate("/Ranking")}>Ranking</span>
+            <img className="Icono" src="../src/Images/RANKING.png" alt="IconoRanking" />
+            <span className='Titulo' onClick={() => navigate("/Ranking")}>Ranking</span>
           </li>
+          <div class="linea-vertical"></div>
           <li className="menu-item">
-            <img src="..\src\Images\CONTACTENOS.png" alt="IconoAjustes" />
-            <span onClick={() => navigate("/Contactenos")}>Contactenos</span>
+            <img className="Icono" src="../src/Images/CONTACTENOS.png" alt="IconoContactenos" />
+            <span className='Titulo' onClick={() => navigate("/Contactenos")}>Contactenos</span>
           </li>
+          <div class="linea-vertical"></div>
           <li className="menu-item">
-            <img src="..\src\images\Iconos Menu Empleado\Log out.png" alt="IconoLogOut" />
-            <span onClick={() => navigate("/")}>Log Out</span>
+            <img className="Icono" src="../src/Images/LOCKOUT.png" alt="IconoLogout" />
+            <span className='Titulo' onClick={() => navigate("/")}>Log Out</span>
           </li>
         </ul>
       </main>
-    
-
     </div>
   )
 }
