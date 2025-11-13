071d6952f11d7125a6c2cfa53d9a11177651d145
Fer-2202 <imarin.marin2003fwd@gmail.com>
2025-11-13 10:43:40 -0600

    Se resolvió el error en el backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
 
 class Resultado(models.Model):
 	id_resultado = models.AutoField(primary_key=True)
