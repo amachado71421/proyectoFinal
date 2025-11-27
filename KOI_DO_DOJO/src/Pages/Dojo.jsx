9d342caf51ff55c538b30c244a50378613f34a47
amachado71421 <alemachadofwd@gmail.com>
2025-11-27 09:03:21 -0600

    Se añadio pagina dojo

--- KOI_DO_DOJO/src/Pages/Dojo.jsx
+++ KOI_DO_DOJO/src/Pages/Dojo.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import BarraMenu from '../Components/BarraMenu'
+import LayerDojo1 from '../Components/LayerDojo/LayerDojo1.JSX'
+import '/src/Styles/Dojo.css';
 
 function Dojo() {
     return (
         <div>
             <BarraMenu />
+            <div className='EspacioCarrusel'></div>
+            <LayerDojo1/>
         </div>
     )
 }
