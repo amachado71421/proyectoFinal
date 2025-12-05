56e0828e21c73309b1e9190587b01a96d1420e6e
amachado71421 <alemachadofwd@gmail.com>
2025-12-05 09:20:44 -0600

    Merge con la version de isma

--- backend/backendKoiDo/settings.py
+++ backend/backendKoiDo/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'koido_dojo',
         'USER': 'root',
-        'PASSWORD': '12345678',
+        'PASSWORD': 'root',
         # use 127.0.0.1 to avoid socket/host resolution issues on Windows
         'HOST': 'localhost',
         'PORT': '3306',
